92af425a38254d68e7a590381c3b499fe839f2b5
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-04-14 13:43:00 +0200

    updated authorization config.

--- config/authorization/config.ex
+++ config/authorization/config.ex
@@ -65,13 +65,27 @@ defmodule Acl.UserGroups.Config do
                         "http://mu.semte.ch/vocabularies/ext/supervision/DocumentAuthenticityType",
                         "http://mu.semte.ch/vocabularies/ext/FileAddress",
                         "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+                        "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject",
                         "http://www.w3.org/ns/prov#Location",
                         "http://mu.semte.ch/vocabularies/ext/BestuurseenheidClassificatieCode",
                         "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
                         "http://mu.semte.ch/vocabularies/ext/BestuursorgaanClassificatieCode",
                         "http://data.vlaanderen.be/ns/besluit#Bestuurseenheid",
                         "http://mu.semte.ch/vocabularies/ext/supervision/InzendingVoorToezichtMelding",
-                        "http://mu.semte.ch/vocabularies/ext/supervision/MeldingStatus"
+                        "http://mu.semte.ch/vocabularies/ext/supervision/MeldingStatus",
+                        "http://lblod.data.gift/vocabularies/employee/LegalStatus",
+                        "http://mu.semte.ch/vocabularies/ext/ChartOfAccount",
+                        "http://mu.semte.ch/vocabularies/ext/AuthenticityType",
+                        "http://mu.semte.ch/vocabularies/ext/TaxType",
+                        "http://mu.semte.ch/vocabularies/ext/SubmissionDocumentStatus",
+                        "http://www.w3.org/2004/02/skos/core#ConceptScheme",
+                        "http://www.w3.org/2004/02/skos/core#Concept",
+                        "http://xmlns.com/foaf/0.1/Document",
+                        "http://rdf.myexperiment.org/ontologies/base/Submission",
+                        "http://mu.semte.ch/vocabularies/ext/SubmissionDocument",
+                        "http://lblod.data.gift/vocabularies/besluit/TaxRate",
+                        "http://lblod.data.gift/vocabularies/automatische-melding/FormData",
+                        "http://mu.semte.ch/vocabularies/ext/Vendor"
                       ] } } ] },
 
       # // CLEANUP
