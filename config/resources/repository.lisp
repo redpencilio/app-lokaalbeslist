fce0958c5a6177394dea6c227c02dac6fce844ed
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-04-23 15:46:24 +0200

    fixed small prefix bug in repository.lisp.

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -37,5 +37,5 @@
 (add-prefix "lblodBesluit" "http://lblod.data.gift/vocabularies/besluit/")
 (add-prefix "muAccount" "http://mu.semte.ch/vocabularies/account/")
 (add-prefix "elod" "http://linkedeconomy.org/ontology#")
-(add-prefix "rdfs" "https://www.w3.org/2000/01/rdf-schema#")
+(add-prefix "rdfs" "http://www.w3.org/2000/01/rdf-schema#")
 (add-prefix "sign" "http://mu.semte.ch/vocabularies/ext/signing/")
