fd28a682587ffe27332f8d0994115311a31c8366
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-04-27 17:36:31 +0200

    small fix for inzending-voor-toezicht resource.

--- config/resources/slave-toezicht-domain.lisp
+++ config/resources/slave-toezicht-domain.lisp
@@ -3,6 +3,7 @@
 
 (define-resource inzending-voor-toezicht ()
   :class (s-prefix "toezicht:InzendingVoorToezicht") ;; subclass of nie:InformationElement > nfo:DataContainer
+  :properties `()
   :resource-base (s-url "http://data.lblod.info/inzendingen-voor-toezicht/")
   :features `(no-pagination-defaults include-uri)
   :on-path "inzendingen-voor-toezicht")
