9ab623d6639e0a4e46a9ea56d61cf5c600017760
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-04-28 15:33:03 +0200

    add skos:notation to concept resource

--- config/resources/master-submissions-domain.lisp
+++ config/resources/master-submissions-domain.lisp
@@ -135,7 +135,8 @@
 
 (define-resource concept ()
   :class (s-prefix "skos:Concept")
-  :properties `((:label :string ,(s-prefix "skos:prefLabel")))
+  :properties `((:label :string ,(s-prefix "skos:prefLabel"))
+                (:notation :string ,(s-prefix "skos:notation")))
   :has-many `((concept-scheme :via ,(s-prefix "skos:inScheme")
                               :as "concept-schemes")
               (concept-scheme :via ,(s-prefix "skos:topConceptOf")
