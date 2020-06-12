7d70ca116bc050e2580b11f6d8f7297418b725c1
Dries Hugaerts <dries.hugaerts@live.be>
2020-06-12 11:18:00 +0200

    added rule for status changes on submission-review.

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -1,2 +1,23 @@
 export default [
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://lblod.data.gift/concepts/d72999bf-2ef4-4332-bd6e-49c8a9e2498e'
+      }
+    },
+    callback: {
+      method: 'POST',
+      url: 'http://search/update',
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
+  }
 ]
