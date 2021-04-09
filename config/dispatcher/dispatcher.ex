7dddb0c231109fe391e4ea9ae221517da049d474
Wout Schellaert <wout.schellaert@gmail.com>
2021-04-09 14:20:00 +0200

    Remove currently unused services

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -14,142 +14,11 @@ defmodule Dispatcher do
   @html %{ accept: %{ html: true } }
 
   ###############################################################
-  # General/Shared
-  ###############################################################
-
-  get "/bestuurseenheden/*path", @json do
-    Proxy.forward conn, path, "http://cache/bestuurseenheden/"
-  end
-  get "/werkingsgebieden/*path", @json do
-    Proxy.forward conn, path, "http://cache/werkingsgebieden/"
-  end
-  get "/bestuurseenheid-classificatie-codes/*path", @json do
-    Proxy.forward conn, path, "http://cache/bestuurseenheid-classificatie-codes/"
-  end
-  get "/bestuursorganen/*path", @json do
-    Proxy.forward conn, path, "http://cache/bestuursorganen/"
-  end
-  get "/bestuursorgaan-classificatie-codes/*path", @json do
-    Proxy.forward conn, path, "http://cache/bestuursorgaan-classificatie-codes/"
-  end
-  get "/personen/*path", @json do
-    Proxy.forward conn, path, "http://cache/personen/"
-  end
-  get "/gebruikers/*path", @json do
-    Proxy.forward conn, path, "http://cache/gebruikers/"
-  end
-  get "/files/:id/download", @any do
-    Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
-  end
-  get "/files/*path", @json do
-    Proxy.forward conn, path, "http://resource/files/"
-  end
-
-  ###############################################################
   # Searching
   ###############################################################
 
   get "/search/*path", @json do
     Proxy.forward conn, path, "http://search/"
-  end
-  get "/search-queries/*path", @json do
-    Proxy.forward conn, path, "http://resource/search-queries/"
-  end
-  post "/search-queries/*path", @json do
-    Proxy.forward conn, path, "http://resource/search-queries/"
-  end
-  get "/search-queries/*path", @turtle do
-    Proxy.forward conn, path, "http://search-query-management/search-queries/"
-  end
-  put "/search-queries/*path", @turtle do
-    Proxy.forward conn, path, "http://search-query-management/search-queries/"
-  end
-  delete "/search-queries/*path", @turtle do
-    Proxy.forward conn, path, "http://search-query-management/search-queries/"
-  end
-  match "/search-query-forms/*path", @turtle do
-    Proxy.forward conn, path, "http://search-query-management/search-query-forms/"
-  end
-
-  ###############################################################
-  # Registration and login
-  ###############################################################
-
-  match "/accounts/*path", @json do
-    Proxy.forward conn, path, "http://resource/accounts/"
-  end
-  match "/gebruikers/*path", @json do
-    Proxy.forward conn, path, "http://resource/gebruikers/"
-  end
-  match "/sessions/*path", @json do
-    Proxy.forward conn, path, "http://login/sessions/"
-  end
-  match "/mock/sessions/*path", @json do
-    Proxy.forward conn, path, "http://mocklogin/sessions/"
-  end
-
-  #################################################################
-  # Submissions
-  #################################################################
-
-  get "/remote-urls/*path", @json do
-    Proxy.forward conn, path, "http://cache/remote-urls/"
-  end
-
-  get "/inzendingen-voor-toezicht/*path", @json do
-    Proxy.forward conn, path, "http://cache/inzendingen-voor-toezicht/"
-  end
-
-  get "/submissions/*path", @json do
-    Proxy.forward conn, path, "http://cache/submissions/"
-  end
-
-  get "/authenticity-types/*path", @json do
-    Proxy.forward conn, path, "http://cache/authenticity-types/"
-  end
-
-  get "/tax-types/*path", @json do
-    Proxy.forward conn, path, "http://cache/tax-types/"
-  end
-
-  get "/chart-of-accounts/*path", @json do
-    Proxy.forward conn, path, "http://cache/chart-of-accounts/"
-  end
-
-  get "/submission-document-statuses/*path", @json do
-    Proxy.forward conn, path, "http://cache/submission-document-statuses/"
-  end
-
-  get "/submission-forms/*path", @json do
-    Proxy.forward conn, path, "http://enrich-submission/submission-documents/"
-  end
-
-  get "/submission-documents/*path", @json do
-    Proxy.forward conn, path, "http://cache/submission-documents/"
-  end
-
-  get "/form-data/*path", @json do
-    Proxy.forward conn, path, "http://cache/form-data/"
-  end
-
-  get "/concept-schemes/*path", @json do
-    Proxy.forward conn, path, "http://cache/concept-schemes/"
-  end
-
-  get "/concepts/*path", @json do
-    Proxy.forward conn, path, "http://cache/concepts/"
-  end
-
-  #################################################################
-  # Review
-  #################################################################
-
-  get "/submission-review-statuses/*path", @json do
-    Proxy.forward conn, path, "http://cache/submission-review-statuses/"
-  end
-
-  match "/submission-reviews/*path", @json do
-    Proxy.forward conn, path, "http://cache/submission-reviews/"
   end
 
   ###############################################################
@@ -157,25 +26,6 @@ defmodule Dispatcher do
   ###############################################################
   get "/favicon.ico", @any do
     send_resp( conn, 404, "" )
-  end
-
-  get "/assets/*path", @any do
-    forward conn, path, "http://toezicht-abb/assets/"
-  end
-
-  get "/@appuniversum/*path", @any do
-    forward conn, path, "http://toezicht-abb/@appuniversum/"
-  end
-
-  match "/authorization/callback" , @html do
-    # For ACM/IDM login and torii
-    forward conn, [], "http://toezicht-abb/torii/redirect.html"
-  end
-
-  match "/*_path", @html do
-    # *_path allows a path to be supplied, but will not yield
-    # an error that we don't use the path variable.
-    forward conn, [], "http://toezicht-abb/index.html"
   end
 
   #################################################################
--- config/resources/slave-toezicht-domain.lisp
+++
@@ -1,9 +0,0 @@
-;; Association resource which should describe properties associated with the form to use
-;; now only 'validity period' is specified, but could be extended with specific bestuursorgaan/eenheid/etc..
-
-(define-resource inzending-voor-toezicht ()
-  :class (s-prefix "toezicht:InzendingVoorToezicht") ;; subclass of nie:InformationElement > nfo:DataContainer
-  :properties `()
-  :resource-base (s-url "http://data.lblod.info/inzendingen-voor-toezicht/")
-  :features `(no-pagination-defaults include-uri)
-  :on-path "inzendingen-voor-toezicht")
