3ed2e5fce5cc4ed324468bf0780fc4da8668ba9a
Wout Schellaert <wout.schellaert@gmail.com>
2021-04-29 11:46:21 +0200

    Add resource service with besluit domain

---
+++ config/resources/domain.lisp
@@ -0,0 +1,14 @@
+(in-package :mu-cl-resources)
+
+(defparameter *cache-count-queries* nil)
+(defparameter *supply-cache-headers-p* t
+  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
+(setf *cache-model-properties-p* t)
+
+(defparameter *include-count-in-paginated-responses* t
+  "when non-nil, all paginated listings will contain the number
+   of responses in the result object's meta.")
+(defparameter *max-group-sorted-properties* nil)
+(defparameter sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
+
+(read-domain-file "master-besluit-domain.lisp")
